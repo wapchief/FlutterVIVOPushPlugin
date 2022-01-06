6599f26d1e124dc5b741c047e3dd6fbe0aa395d3
吴冰 <wubing@fuliaoyi.com>
2022-01-06 15:37:30 +0800

    update plugin

--- android/src/main/java/com/wapchief/flutter_vivo_push_plugin/FlutterVivoPushPlugin.java
+++ android/src/main/java/com/wapchief/flutter_vivo_push_plugin/FlutterVivoPushPlugin.java
@@ -1,15 +1,23 @@
 package com.wapchief.flutter_vivo_push_plugin;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
 import com.vivo.push.IPushActionListener;
 import com.vivo.push.PushClient;
+import com.vivo.push.ups.TokenResult;
+import com.vivo.push.ups.UPSRegisterCallback;
+import com.vivo.push.ups.VUpsManager;
 import com.vivo.push.util.VivoPushException;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -18,13 +26,16 @@ import io.flutter.plugin.common.MethodChannel.Result;
 /**
  * FlutterVivoPushPlugin
  */
-public class FlutterVivoPushPlugin implements FlutterPlugin, MethodCallHandler {
+public class FlutterVivoPushPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
     /// The MethodChannel that will the communication between Flutter and native Android
     ///
     /// This local reference serves to register the plugin with the Flutter Engine and unregister it
     /// when the Flutter Engine is detached from the Activity
     private MethodChannel channel;
-    private Application mApplication;
+    private static Application mApplication;
+    private static Activity mActivity;
+
+    private static String TAG = "FlutterVivoPushPlugin";
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
@@ -35,25 +46,40 @@ public class FlutterVivoPushPlugin implements FlutterPlugin, MethodCallHandler {
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (!Build.MANUFACTURER.toLowerCase().contains("vivo")) {
+            Log.d(TAG, "Not vivo push mobile devices !!!");
+            return;
+        }
         switch (call.method) {
             case "initSDK":
-                Log.d("vivo-inSDK", "");
-                //初始化sdk
                 try {
+                    Log.d(TAG, "-----vivo-openPush-mApplication:" + mApplication.getBaseContext().toString());
+                    //初始化sdk
                     PushClient.getInstance(mApplication).initialize();
+                    //打开推送
+                    VUpsManager.getInstance().turnOnPush(mApplication, codeResult -> {
+                        //code:101系统不支持
+                        if (codeResult.getReturnCode() == 101) {
+                            Log.e(TAG, "-----vivo-openPush-error:101系统不支持");
+                        } else {
+                            Log.v(TAG,
+                                    "-----vivo-openPush-codeResult:" + codeResult.getReturnCode());
+                        }
 
-                } catch (VivoPushException e) {
-                    Log.d("vivo-init-error", e.toString());
+                    });
+                } catch (Exception e) {
+                    Log.e(TAG, e.toString());
                 }
+                break;
+            case "getRegId":
+                result.success(PushClient.getInstance(mApplication).getRegId());
                 break;
             case "openPush":
                 //打开推送
-                Log.d("vivo-openPush", "");
-                PushClient.getInstance(mApplication).turnOnPush(state -> {
-                    Log.d("vivo-openPush-state:", state + "");
-                    if (state == 0) {
-                        Log.d("vivo-pushId:", PushClient.getInstance(mApplication).getRegId());
-                    }
+                VUpsManager.getInstance().turnOnPush(mApplication, codeResult -> {
+                    //code:101系统不支持
+                    Log.v(TAG, "-----vivo-openPush-codeResult:" + codeResult.getReturnCode());
+
                 });
                 break;
             case "offPush":
@@ -61,12 +87,25 @@ public class FlutterVivoPushPlugin implements FlutterPlugin, MethodCallHandler {
                 PushClient.getInstance(mApplication).turnOffPush(new IPushActionListener() {
                     @Override
                     public void onStateChanged(int state) {
-
+                        Log.v(TAG, "-----vivo-closePush-state:" + state + "");
                     }
                 });
                 break;
+            case "registerToken":
+                String appId = call.argument("appId");
+                String appKey = call.argument("appKey");
+                String secret = call.argument("secret");
+                VUpsManager.getInstance().registerToken(mApplication, appId, appKey,
+                        secret, tokenResult -> {
+                            if (tokenResult.getReturnCode() == 0) {
+                                Log.v(TAG, "registerToken 注册成功 regID = " + tokenResult.getToken());
+                            } else {
+                                Log.v(TAG, "registerToken 注册失败");
+                            }
+                        });
+                break;
             default:
-                Log.d("vivo-FlutterVivoPushPluginr", call.method.toString());
+                Log.d(TAG, "------method:" + call.method.toString());
                 result.notImplemented();
                 break;
         }
@@ -75,5 +114,25 @@ public class FlutterVivoPushPlugin implements FlutterPlugin, MethodCallHandler {
     @Override
     public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
         channel.setMethodCallHandler(null);
+    }
+
+    @Override
+    public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+        mActivity = binding.getActivity();
+    }
+
+    @Override
+    public void onDetachedFromActivityForConfigChanges() {
+
+    }
+
+    @Override
+    public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+    }
+
+    @Override
+    public void onDetachedFromActivity() {
+
     }
 }
--- lib/flutter_vivo_push_plugin.dart
+++ lib/flutter_vivo_push_plugin.dart
@@ -3,27 +3,32 @@ import 'dart:io';
 
 import 'package:flutter/services.dart';
 export 'package:flutter_vivo_push_plugin/flutter_vivo_push_plugin.dart';
+
 class FlutterVivoPushPlugin {
   static const MethodChannel _channel = MethodChannel('flutter_vivo_push_plugin');
 
   static initSDK() {
-    // if (Platform.isIOS) {
-    //   return;
-    // }
     _channel.invokeMethod('initSDK');
   }
 
   static openPush() {
-    // if (Platform.isIOS) {
-    //   return;
-    // }
     _channel.invokeMethod('openPush');
   }
 
   static offPush() {
-    if (Platform.isIOS) {
-      return;
-    }
     _channel.invokeMethod('offPush');
+  }
+
+  static Future<String> getRegId() async {
+    return await _channel.invokeMethod('getRegId');
+  }
+
+  static registerOPPOToken(
+      {required String appId, required String appKey, required String secret}) {
+    _channel.invokeMethod('registerToken', {
+      'appId': appId,
+      'appKey': appKey,
+      'secret': secret,
+    });
   }
 }
